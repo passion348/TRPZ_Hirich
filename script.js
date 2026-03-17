8aed7da3de7695cf1193c0d4efe4370fa86f7cf7
passion348 <git config --global user.email jeny.girich@gmail.comgit config --global user.email>
2026-03-17 15:08:35 +0200

    game-site

---
+++ script.js
@@ -0,0 +1,50 @@
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+const maxAttempts = 5;
+
+function checkGuess() {
+  const userGuess = Number(document.getElementById("guess").value);
+  const message = document.getElementById("message");
+  const attemptsText = document.getElementById("attempts");
+
+  if (!userGuess) {
+    message.innerText = "Введіть число!";
+    return;
+  }
+
+  if (attempts >= maxAttempts) {
+    message.innerText = "Спроби закінчились! Натисніть 'Заново'";
+    return;
+  }
+
+  attempts++;
+
+  if (userGuess === secretNumber) {
+    message.innerText = `Ви вгадали число за ${attempts} спроб!`;
+    attemptsText.innerText = "";
+    return;
+  }
+
+  if (attempts === maxAttempts) {
+    message.innerText = `Ви програли! Загадане число було: ${secretNumber}`;
+    attemptsText.innerText = "";
+    return;
+  }
+
+  if (userGuess > secretNumber) {
+    message.innerText = "Загадане число менше!";
+  } else {
+    message.innerText = "Загадане число більше!";
+  }
+
+  attemptsText.innerText = `Спроба: ${attempts} / ${maxAttempts}`;
+}
+
+function resetGame() {
+  secretNumber = Math.floor(Math.random() * 100) + 1;
+  attempts = 0;
+
+  document.getElementById("message").innerText = "";
+  document.getElementById("attempts").innerText = "";
+  document.getElementById("guess").value = "";
+}
